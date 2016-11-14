2ac0661ec3f0ac8195eb8184ae008200a3ae950c
X-Y-1223 <ningxiaoyu@CNSHAMD0025-2.local>
2016-11-14 17:24:55 +0800

    111

---
+++ wechat/Request.php
@@ -0,0 +1,39 @@
+<?php
+	function getJsSDK($url)
+	{
+
+		$time=file_get_contents("upload/time.txt");
+		$ticket=file_get_contents("upload/ticket.txt");
+		if (!$time || (time() - $time >= 3600)){
+			$rs = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.APPID .'&secret=' . APPSECRET);
+			$rs = json_decode($rs,true);
+			if(isset($rs['access_token'])){
+				$time = time();
+				$access_token = $rs['access_token'];
+				$ticketfile = file_get_contents("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi");
+				$ticketfile = json_decode($ticketfile, true);
+				$ticket = $ticketfile['ticket'];
+				$fp = fopen("upload/time.txt", "w");
+				fwrite($fp,$time);
+				fclose($fp);
+				$fp = fopen("upload/access_token.txt", "w");
+				fwrite($fp,$access_token);
+				fclose($fp);
+				$fp = fopen("upload/ticket.txt", "w");
+				fwrite($fp,$ticket);
+				fclose($fp);
+			}else{
+				throw new Exception($rs['errcode']);
+			}
+		}
+		$str = '1234567890abcdefghijklmnopqrstuvwxyz';
+		$noncestr = '';
+		for($i=0;$i<8;$i++){
+			$randval = mt_rand(0,35);
+			$noncestr .= $str[$randval];
+		}
+		$ticketstr="jsapi_ticket=". $ticket ."&noncestr=". $noncestr ."&timestamp=". $time ."&url=". $url;
+		$sign = sha1($ticketstr);
+		return json_encode(array("appid" => APPID, "time" => $time, "noncestr" => $noncestr, "sign" => $sign, "url" => $url));	
+	}
+?>
---
+++ wechat/js/wechat.js
@@ -0,0 +1,161 @@
+var wechatParameter = {};
+
+var shareData = {
+    title: '定制属于您的圣诞愿景',
+    desc: '浓情佳节 挚爱之选',
+    descTimeline: '定制属于您的圣诞愿景',
+    link: window.location.host,
+    imgUrl: 'http://' + window.location.host + '/images/loading.jpg',
+    returnFun: function(){
+
+    }
+}
+
+function wechatFun(){
+    $.ajax({
+        type: "POST",
+        url: "/Request.php?model=jssdk&url=" + encodeURIComponent(window.location.href),
+        dataType:"json"
+    }).done(function(data){
+        wechatParameter = {
+            "_appid":data.appid,
+            "_time": data.time,
+            "_nonceStr": data.noncestr,
+            "_sign": data.sign
+        }
+        wechatShare();
+    }).fail(function() {
+        console.log("请求接口失败！");
+    });
+}
+
+
+
+function wechatShare(){
+  wx.config({
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: wechatParameter._appid, // 必填，公众号的唯一标识
+      timestamp: wechatParameter._time, // 必填，生成签名的时间戳
+      nonceStr: wechatParameter._nonceStr, // 必填，生成签名的随机串
+      signature: wechatParameter._sign,// 必填，签名，见附录1
+      jsApiList: [
+        'checkJsApi',
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'onMenuShareQQ',
+        'onMenuShareWeibo',
+        'hideMenuItems',
+        'showMenuItems',
+        'hideAllNonBaseMenuItem',
+        'showAllNonBaseMenuItem',
+        'translateVoice',
+        'startRecord',
+        'stopRecord',
+        'onRecordEnd',
+        'playVoice',
+        'pauseVoice',
+        'stopVoice',
+        'uploadVoice',
+        'downloadVoice',
+        'chooseImage',
+        'previewImage',
+        'uploadImage',
+        'downloadImage',
+        'getNetworkType',
+        'openLocation',
+        'getLocation',
+        'hideOptionMenu',
+        'showOptionMenu',
+        'closeWindow',
+        'scanQRCode',
+        'chooseWXPay',
+        'openProductSpecificView',
+        'addCard',
+        'chooseCard',
+        'openCard'
+      ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  });
+  
+  wx.ready(function(){
+        wx.onMenuShareTimeline({
+            title: shareData.descTimeline, // 分享标题
+            link: shareData.link, // 分享链接
+            imgUrl: shareData.imgUrl, // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                shareData.returnFun();
+                //alert('分享成功');
+            },
+            cancel: function () { 
+                // 用户取消分享后执行的回调函数
+                shareData.returnFun();
+                //alert("分享失败")
+            }
+        });
+        
+        
+        wx.onMenuShareAppMessage({
+            title: shareData.title, // 分享标题
+            link: shareData.link, // 分享链接
+            imgUrl: shareData.imgUrl, // 分享图标
+            desc: shareData.desc,
+            success: function () { 
+                // 用户确认分享后执行的回调函数
+                shareData.returnFun();
+                //alert('分享成功');
+            },
+            cancel: function () { 
+                // 用户取消分享后执行的回调函数
+                shareData.returnFun();
+                //alert("分享失败")
+            }
+        });
+  })
+
+}
+
+
+
+function editShare(){   ///
+     wx.onMenuShareTimeline({
+            title: shareData.descTimeline, // 分享标题
+            link: shareData.link, // 分享链接
+            imgUrl: shareData.imgUrl, // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                shareData.returnFun();
+                //alert('分享成功');
+            },
+            cancel: function () { 
+                // 用户取消分享后执行的回调函数
+                shareData.returnFun();
+                //alert("分享失败")
+            }
+        });
+        
+        
+        wx.onMenuShareAppMessage({
+            title: shareData.title, // 分享标题
+            link: shareData.link, // 分享链接
+            imgUrl: shareData.imgUrl, // 分享图标
+            desc: shareData.desc,
+            success: function () { 
+                // 用户确认分享后执行的回调函数
+                shareData.returnFun();
+                //alert('分享成功');
+            },
+            cancel: function () { 
+                // 用户取消分享后执行的回调函数
+                shareData.returnFun();
+                //alert("分享失败")
+            }
+        });
+}
+
+
+
+
+
+wechatFun();
+
+
